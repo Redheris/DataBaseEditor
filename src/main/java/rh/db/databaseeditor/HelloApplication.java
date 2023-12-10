e4b3f1e2d55a35cde77154a1845001742da7f821
Anatoly Solomonov <solomonov010@mail.ru>
2023-12-10 20:05:44 +0400

    Added authentication to a database

--- src/main/java/rh/db/databaseeditor/HelloApplication.java
+++ src/main/java/rh/db/databaseeditor/HelloApplication.java
@@ -10,38 +10,42 @@ import java.sql.*;
 
 public class HelloApplication extends Application {
 
-    private static final String URL =
-            "jdbc:sqlserver://REDHERISPC;" +
-            "databaseName=BOOKSHOP;integratedSecurity=true;" +
-            "encrypt=true;trustServerCertificate=true";
+    private static Connection connection;
+    private static Statement statement;
+
+    public static void main(String[] args) {
+        launch();
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
         ResultSet resultSet = null;
 
-        try (Connection connection = DriverManager.getConnection(URL);
-             Statement statement = connection.createStatement();) {
-            // Create and execute a SELECT SQL statement.
-            String selectSql = "SELECT TOP 10 id_customer, fname, lname from Customer";
-            resultSet = statement.executeQuery(selectSql);
-
-            // Print results from select statement
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
-            }
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Bookshop Database Editor");
         stage.setScene(scene);
+        stage.setMinWidth(666);
+        stage.setMinHeight(450);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
+    // Сеттеры для connection и statement
+    protected static void setConnection(Connection c) {
+        connection = c;
+    }
+
+    protected static void setStatement(Statement st) {
+        statement = st;
+    }
+
+    protected static void logout() {
+        try {
+            // Закрытие соединения
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/rh/db/databaseeditor/HelloController.java
+++ src/main/java/rh/db/databaseeditor/HelloController.java
@@ -1,14 +1,86 @@
 package rh.db.databaseeditor;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    public HBox login_box;
+    @FXML
+    private VBox loginInfo;
+    @FXML
+    private Label usernameInfo, dbNameInfo;
+    @FXML
+    private TextField dbName, username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button logout;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onLoginButtonClick() {
+        Alert resultInfo = new Alert(Alert.AlertType.INFORMATION);
+        final String URL =
+                "jdbc:sqlserver://localhost;encrypt=true;trustServerCertificate=true;" +
+                "databaseName=" + dbName.getText() + ";" +
+                "username=" + username.getText() + ";" +
+                "password=" + password.getText() + ";";
+        try (Connection connection = DriverManager.getConnection(URL);
+             Statement statement = connection.createStatement()) {
+            HelloApplication.setConnection(connection);
+            HelloApplication.setStatement(statement);
+
+            // Скрываем форму авторизации
+            login_box.setVisible(false);
+            password.setText(null);
+
+            resultInfo.setTitle("Авторизация прошла успешно");
+            resultInfo.setHeaderText(
+                    "Авторизация пользователя \"" + username.getText() +
+                    "\" в базе данных \"" + dbName.getText() + "\" прошла успешно"
+            );
+            resultInfo.showAndWait();
+            logout.setDisable(false);
+            usernameInfo.setText(username.getText());
+            dbNameInfo.setText(dbName.getText());
+            loginInfo.prefWidth(100);
+            loginInfo.setVisible(true);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            resultInfo.setTitle("Ошибка авторизации");
+            resultInfo.setAlertType(Alert.AlertType.ERROR);
+            resultInfo.setHeaderText(
+                    "Произошла ошибка при авторизации пользователя \"" + username.getText() +
+                    "\" в базе данных \"" + dbName.getText() + "\""
+            );
+            resultInfo.setContentText("Текст ошибки: \n" + e.getMessage());
+            resultInfo.showAndWait();
+        }
+    }
+
+    public void onLogoutButtonClick() {
+        // Отключение от базы данных
+        HelloApplication.logout();
+
+        // Отображаем форму авторизации
+        login_box.setVisible(true);
+
+        // Сбрасываем данные о соединении
+        HelloApplication.setConnection(null);
+        HelloApplication.setStatement(null);
+        usernameInfo.setText("NONE");
+        dbNameInfo.setText("NONE");
+        logout.setDisable(true);
+        loginInfo.setVisible(false);
+        loginInfo.prefWidth(0);
     }
 }
